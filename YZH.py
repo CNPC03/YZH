4f72d3aec50c7bf1a4d4573b9a4612357f3aa9af
Gael Huk Kukla <kyerui_galkukla@outlook.com>
2024-06-07 14:51:06 -0300

    YZH

---
+++ YZH.py
@@ -0,0 +1,26 @@
+def gcd(a, b):
+    while b != 0:
+        a, b = b, a % b
+    return a
+
+def mod_inverse(a, m):
+    m0, x0, x1 = m, 0, 1
+    while a > 1:
+        q = a // m
+        m, a = a % m, m
+        x0, x1 = x1 - q * x0, x0
+    return x1 + m0 if x1 < 0 else x1
+
+def decrypt(private_key, ciphertext):
+    d, n = private_key
+    plain = [chr(pow(char, d, n)) for char in ciphertext]
+    return ''.join(plain)
+
+
+private_key = (fvby, rlf)
+
+
+mensagem = "Lualy lujyfwalk tlzzhnl olyl"
+
+decrypted_message = decrypt(private_key, mensagem)
+print("Kljyfwalk tlzzhnl:", decrypted_message)
